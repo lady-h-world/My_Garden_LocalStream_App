1786d1a21d18f8c1a8175e0ee32a24b116ae6513
hanhanwu <wuhanhan999@gmail.com>
2024-05-03 19:29:48 -0400

    add google  when Apify can't work

--- pages/results.py
+++ pages/results.py
@@ -49,50 +49,50 @@ if 'country' in st.session_state.keys() and st.session_state.country != '' and \
         with st.spinner('🚀 Collecting local activities! Look 👇'):
             extra_query_str = f'in {st.session_state.month}'
 
-        try:  # run Apify first
-            apify_client = ApifyClient(st.secrets['APIFY_TOKEN'])
-            query_lst = []
-            for suggestion in suggestion_lst:
-                query_lst.append(f'{suggestion} {extra_query_str}')
+            try:  # run Apify first
+                apify_client = ApifyClient(st.secrets['APIFY_TOKEN'])
+                query_lst = []
+                for suggestion in suggestion_lst:
+                    query_lst.append(f'{suggestion} {extra_query_str}')
 
-            run_input = {
-                "queries": query_lst,
-                "maxResultsPerQuery": MAX_QUERY_CT,
-            }
-            run = apify_client.actor("tnudF2IxzORPhg4r8").call(run_input=run_input)
-            pre_query = None
-            image_lst = []
-            for item in apify_client.dataset(run["defaultDatasetId"]).iterate_items():
-                cur_query = item['query']
-                if cur_query != pre_query:
-                    if len(image_lst) > 0:
-                        display_images(pre_query.replace(extra_query_str, ''), MAX_QUERY_CT, image_lst)
-                    pre_query = cur_query
-                    image_lst = []
-                try:
-                    response = requests.get(item['imageUrl'], timeout=2)
-                    if response.status_code == 200:
-                        image_lst.append(Image.open(BytesIO(response.content)))
-                except:
-                    pass
-
-            if len(image_lst) > 0:
-                display_images(pre_query.replace(extra_query_str, ''), MAX_QUERY_CT, image_lst)
-        except:  # run GCS if Apify doesn't work
-            google_api_key = st.secrets['GOOGLE_API_KEY']
-            cx = st.secrets['GOOGLE_EX_ID']
-            for suggestion in suggestion_lst:
-                query = f'{suggestion}'
-                image_urls = search_images(query, google_api_key, cx, hq=extra_query_str)
+                run_input = {
+                    "queries": query_lst,
+                    "maxResultsPerQuery": MAX_QUERY_CT,
+                }
+                run = apify_client.actor("tnudF2IxzORPhg4r8").call(run_input=run_input)
+                pre_query = None
                 image_lst = []
-                for image_url in image_urls:
+                for item in apify_client.dataset(run["defaultDatasetId"]).iterate_items():
+                    cur_query = item['query']
+                    if cur_query != pre_query:
+                        if len(image_lst) > 0:
+                            display_images(pre_query.replace(extra_query_str, ''), MAX_QUERY_CT, image_lst)
+                        pre_query = cur_query
+                        image_lst = []
                     try:
-                        response = requests.get(image_url, timeout=2)
+                        response = requests.get(item['imageUrl'], timeout=2)
                         if response.status_code == 200:
                             image_lst.append(Image.open(BytesIO(response.content)))
                     except:
                         pass
+
                 if len(image_lst) > 0:
-                    display_images(suggestion, MAX_QUERY_CT, image_lst)
+                    display_images(pre_query.replace(extra_query_str, ''), MAX_QUERY_CT, image_lst)
+            except:  # run GCS if Apify doesn't work
+                google_api_key = st.secrets['GOOGLE_API_KEY']
+                cx = st.secrets['GOOGLE_EX_ID']
+                for suggestion in suggestion_lst:
+                    query = f'{suggestion}'
+                    image_urls = search_images(query, google_api_key, cx, hq=extra_query_str)
+                    image_lst = []
+                    for image_url in image_urls:
+                        try:
+                            response = requests.get(image_url, timeout=2)
+                            if response.status_code == 200:
+                                image_lst.append(Image.open(BytesIO(response.content)))
+                        except:
+                            pass
+                    if len(image_lst) > 0:
+                        display_images(suggestion, MAX_QUERY_CT, image_lst)
 
     st.stop()
