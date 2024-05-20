d5b94078dc72217e6feba7617e2a660d89905a17
lady-h-world <ladyheartsworld@gmail.com>
2024-05-20 09:26:08 -0400

    remove st.stop

--- pages/results.py
+++ pages/results.py
@@ -95,4 +95,3 @@ if 'country' in st.session_state.keys() and st.session_state.country != '' and \
                             pass
                     if len(image_lst) > 0:
                         display_images(suggestion, MAX_QUERY_CT, image_lst)
-    st.stop()
