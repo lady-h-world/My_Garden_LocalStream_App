370bf1d04108a909fdae10c890f9f5eb2791088d
lady-h-world <ladyheartsworld@gmail.com>
2024-06-21 10:02:22 -0400

    resolving apify maintenance issue

--- pages/results.py
+++ pages/results.py
@@ -59,6 +59,7 @@ if 'country' in st.session_state.keys() and st.session_state.country != '' and \
                     "maxResultsPerQuery": MAX_QUERY_CT,
                 }
                 run = apify_client.actor("tnudF2IxzORPhg4r8").call(run_input=run_input, timeout_secs=90)
+                st.write(run['status'])  # TEST ONLY
                 if run['status'] != "SUCCEEDED":
                     raise Exception('Apify run failed')
             with st.spinner('🚀 Loading photos! Look 👇'):
