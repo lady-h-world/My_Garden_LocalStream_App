4c34073f8ffa0148ec12165b5942750bd9204cda
lady-h-world <ladyheartsworld@gmail.com>
2024-06-21 11:26:42 -0400

    resolving apify maintenance issue

--- pages/results.py
+++ pages/results.py
@@ -47,7 +47,7 @@ if 'country' in st.session_state.keys() and st.session_state.country != '' and \
                 st.write("🤔 Can't show them on the map, check suggestions below 👇")
 
     if len(suggestion_lst) > 0:
-        st.write('##')
+        st.write('## test')
         extra_query_str = f'in {st.session_state.month} at {st.session_state.region}, {st.session_state.country}'
         try:  # run Apify
             with st.spinner('🔮 Collecting local activities and most relevant photos!'):
